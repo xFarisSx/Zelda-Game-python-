cf3fe2e9d7c20c72015aad1631be6b31bf51501e
xFarisSx <faristaleek@gmail.com>
2022-12-06 19:56:19 +0300

    first

---
+++ 15 - fixes audio/code/main.py
@@ -0,0 +1,37 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+	def __init__(self):
+
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		pygame.display.set_caption('Zelda')
+		self.clock = pygame.time.Clock()
+
+		self.level = Level()
+
+		# sound 
+
+
+	
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
+
+			self.screen.fill(WATER_COLOR)
+			self.level.run()
+			pygame.display.update()
+			self.clock.tick(FPS)
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
